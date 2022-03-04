5b36050ff8f2c35abe5f3479027a096ff100fab5
Cristyan Henrich <cristyanhenrich16@gmail.com>
2022-03-04 15:04:03 -0300

    Pegando nome correto de arquivo

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const express = require("express");
+ const express = require("express");
 const app = express();
 const multer = require("multer");
 const path = require("path")
 
 app.set('view engine', 'ejs');
 
+var data = new Date();
+var dia = String(data.getDate()).padStart(2, '0');
+var mes = String(data.getMonth() + 1).padStart(2, '0');
+var ano = data.getFullYear();
+dataAtual = dia + mes + ano;
+console.log(dataAtual);
+
 const storage = multer.diskStorage({
     destination: function(req, res, cb){
         cb(null, "uploads/")
     },
     filename: function(req, file, cb){
-        cb(null, file.originalname + Date.now() + path.extname(file.originalname));
+        cb(null, (file.originalname).split('\\').pop().split('.').shift() + ' - ' + dataAtual + path.extname(file.originalname));
     }
 })
 
